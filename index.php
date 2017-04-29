4ac92e8d646ed1a44fa66dd36ce604a6f279cf1b
Cesar Maquera <yushiroche@gmail.com>
2017-04-29 23:35:45 +0000

    Hecho

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
+	<title>PHP Starter Application | Exam</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" />
+	<!--<link rel="stylesheet" href="style.css" />-->
 </head>
 <body>
-	<div class="header center"><img src="../images/header.png" ></div>
-	<div class="content center"><img src="../images/content.png" >
-		<table class="table">
+	<div class="header text-center"><img style="width: 100%;" src="../images/header.png" ></div>
+	<div class="content">
+	    <div class="container">
+    	    <div class="row">
+    	        <div class="col-sm-6">
+            		  <h3><strong>For data scientists</strong></h3>
+                </div>
+                <div class="col-sm-6">
+            		  <h3><strong>For IT professionals</strong></h3>
+                </div>
                 
-                     <?php
+                
+                <div class="col-sm-6">
+            		          
+            		          
+            		          <?php
                     $servername = "us-cdbr-iron-east-03.cleardb.net";
                     $username = "b74ba3320e82ec";
                     $password = "2d194843";
@@ -25,23 +37,85 @@
                     $sql = "SELECT * from servicios";
                     $result = $conn->query($sql);
                     if ($result->num_rows > 0) {
+                        
+                        $contdor = 0;
                         // output data of each row
-                        while($row = $result->fetch_assoc()) {?>
-                        <tr>
-	                        <td><?php echo $row['codigo_servicio']?></td>
-                        	<td><?php echo $row['nombre_servicio']?></td>
-							<td><?php echo $row['descripcion_servicio']?></td>
-							<td><?php echo $row['imagen_servicio']?></td>
-                        </tr>
-                       <?php }
+                        while($row = $result->fetch_assoc()) { $contador++; if ($contador < 4){
+            	            ?>
+                       
+                        
+                            
+            		          <div class="row">
+            		              <div class="col-sm-2"><img src="images/ima1.jpg"></img></div>
+            		              <div class="col-sm-8" style="margin-top: 20px;">
+            		                  <p><strong><?php echo $row['nombre_servicio']?></strong></p>
+            		                  <p><?php echo $row['descripcion_servicio']?></p>
+            		                  
+            		              </div>
+            		          </div>
+            	      
+                        
+                       <?php }                   
+                       }
+                       $contador = 0;
                     } else {
                         echo "0 results";
                     }
                     $conn->close();
                    ?>
-
-               </table>
+            		          
+            		          
+            		          
+            	        </div>
+            	
+            	<div class="col-sm-6">
+                
+    	        <?php
+                    $servername = "us-cdbr-iron-east-03.cleardb.net";
+                    $username = "b74ba3320e82ec";
+                    $password = "2d194843";
+                    $dbname = "ad_bb6ddb2f34daf52";
+                   
+                    // Create connection
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                    $sql = "SELECT * from servicios";
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        
+                        $contdor = 0;
+                        // output data of each row
+                        while($row = $result->fetch_assoc()) { $contador++; if ($contador > 3){
+            	            ?>
+                       
+                        
+                            
+            		          <div class="row">
+            		              <div class="col-sm-2"><img src="images/ima1.jpg"></img></div>
+            		              <div class="col-sm-8" style="margin-top: 20px;">
+            		                  <p><strong><?php echo $row['nombre_servicio']?></strong></p>
+            		                  <p><?php echo $row['descripcion_servicio']?></p>
+            		                  
+            		              </div>
+            		          </div>
+            	      
+                        
+                       <?php }
+                       }
+                       $contador = 0;
+                    } else {
+                        echo "0 results";
+                    }
+                    $conn->close();
+                   ?>
+    	       </div>
+    		</div>
+        </div>
 	</div>
-	<div class="footer center"><img src="../images/footer.png" ></div>	
+	<div class="footer text-center"><img style="width: 100%;" src="../images/footer.png" ></div>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
