d26197145ad5a7c850fd0ef58869eed929566bf5
Cesar Ignacio Maquera Ticona <u201319387@upc.edu.pe>
2017-04-29 21:08:29 +0000

    wuadas

--- index.php
+++ index.php
@@ -7,7 +7,19 @@
 </head>
 <body>
 	<div class="header"><img src="../images/header.png" ></div>
-	<div class="content"><img src="../images/content.png" ></div>
+	<div class="content"><img src="../images/content.png" >
+		<table class="tabla">
+			<tr>
+				<td style='width: 30%;'>
+					<img class = 'newappIcon' src='images/newapp-icon.png'>
+				</td>
+				<td>
+					<h1 id = "message"><?php echo "Hello World!"; ?></h1>
+					<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
+				</td>
+			</tr>
+		</table>
+	</div>
 	<div class="footer"><img src="../images/footer.png" ></div>	
 <!--	<table>
 		<tr>
